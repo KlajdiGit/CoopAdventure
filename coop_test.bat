ec3a675f98477096953f4d0a45ae23cf7cb220a2
Klajdi Hoxhalli <hoxhalli@sheridancollege.ca>
2024-05-20 19:52:10 -0400

    Added the project

---
+++ coop_test.bat
@@ -0,0 +1,1 @@
+"C:\Program Files\Epic Games\UE_5.2\Engine\Binaries\Win64\UnrealEditor.exe" "C:\Users\klajd\Documents\Unreal Projects\CoopAdventure\CoopAdventure.uproject" -game -ResX=960 -ResY=540 -log -WINDOWED
