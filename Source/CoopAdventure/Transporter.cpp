693aa55e9deb4fc314e8f3065ce4819ab74bdee7
Klajdi Hoxhalli <hoxhalli@sheridancollege.ca>
2024-05-24 21:06:27 -0400

    Gave the transporter component moving functionality

--- Source/CoopAdventure/Transporter.cpp
+++ Source/CoopAdventure/Transporter.cpp
@@ -71,5 +71,21 @@ void UTransporter::TickComponent(float DeltaTime, ELevelTick TickType, FActorCom
 		}
 	}
 
+	
+	AActor* MyOwner = GetOwner();
+	if (MyOwner && MyOwner->HasAuthority() && ArePointsSet)
+	{
+		FVector CurrentLocation = MyOwner->GetActorLocation();
+		float Speed = FVector::Distance(StartPoint, EndPoint) / MoveTime;
+
+		FVector TargetLocation = AllTriggerActorsTriggered ? EndPoint : StartPoint;
+
+		if (!CurrentLocation.Equals(TargetLocation))
+		{
+			FVector NewLocation = FMath::VInterpConstantTo(CurrentLocation, TargetLocation, DeltaTime, Speed);
+			MyOwner->SetActorLocation(NewLocation);
+		}
+	}
+
 }
 
