10dfb6854e5b1eaa5b9c3daf7e385ecdd1e3f371
Klajdi Hoxhalli <hoxhalli@sheridancollege.ca>
2024-05-24 21:34:35 -0400

    MovableActor class created

---
+++ Source/CoopAdventure/MovableActor.cpp
@@ -0,0 +1,22 @@
+
+
+#include "MovableActor.h"
+
+AMovableActor::AMovableActor()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+void AMovableActor::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+void AMovableActor::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
